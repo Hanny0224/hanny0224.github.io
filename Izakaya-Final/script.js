23013715941c377e452e0d1b1880734ca61e7963
Hanny0224 <132648668+Hanny0224@users.noreply.github.com>
2023-06-16 01:08:59 +0800

    Add files via upload

--- Izakaya-Final/script.js
+++ Izakaya-Final/script.js
@@ -17,7 +17,30 @@ function openImage(img) {
 	if (event.target == modal) {
 	  modal.style.display = "none";
 	}
-  };
+};
+
+document.getElementById('navbar').addEventListener('click', (e)=>{
+	document.getElementById('navbar').classList.toggle('active');
+})
+
+var slider = tns({
+	container: '.cards',
+	items:1,
+	gutter:20,
+	slideBy:"page",
+	autoplayButton: false,
+	controlsPosition: 'bottom',
+	navPosition: 'bottom',
+	controlsText: ["&laquo prev", "next &raquo"],
+	responsive:{
+		720:{
+			items:2,
+		},
+		980:{
+			items:3,
+		},
+	}
+}); 
 
   
   
